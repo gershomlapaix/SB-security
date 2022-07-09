ad53e555bf58adae328d19475632b3ada0a3c5ec
@nsengiyumv33 <nsengiyumv33@gmail.com>
2022-07-09 23:56:57 +0200

    finish authentication

--- SB-auth3/src/main/java/com/lapaix/SBauth3/security/SecurityConfig.java
+++ SB-auth3/src/main/java/com/lapaix/SBauth3/security/SecurityConfig.java
@@ -3,8 +3,10 @@ package com.lapaix.SBauth3.security;
 import com.lapaix.SBauth3.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,54 +15,45 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * By Gershom
- *                Some of the important notes
- *   1. The "auth" routes can be accessed by all users since register and login is for everyone
- *   2. The "user" routes can be accessed by only authenticated users with the role "USER" which is set by MyUserDetailsService
- *   3. Server will reject the requests as unauthorized when entry point is reached
- *   4. JWTFilter is added to the filter chain in order to process the incoming requests
- *   5. Creating a bean for password encoder
- *   6. Exposing the bean of the authentication manager which will be used to run the authentication process in the "authController"
- * */
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private UserRepo userRepo;
 
-    @Autowired
-    private JWTFilter jwtFilter;
-
-    @Autowired
-    private MyUserDetailsService userDetailsService;
+    @Autowired private UserRepo userRepo;
+    @Autowired private JWTFilter filter;
+    @Autowired private MyUserDetailsService uds;
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception{
+    protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .httpBasic().disable()
                 .cors()
                 .and()
-                .authorizeRequests()
+                .authorizeHttpRequests()
                 .antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/api/user/**").hasRole("USER")
                 .and()
-                .userDetailsService(userDetailsService)
+                .userDetailsService(uds)
                 .exceptionHandling()
-                .authenticationEntryPoint((request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized"))
+                .authenticationEntryPoint(
+                        (request, response, authException) ->
+                                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized")
+                )
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
-        }
+        http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+    }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
     @Override
-    public AuthenticationManager authenticationManager() throws Exception{
-        return super.authenticationManager();
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
