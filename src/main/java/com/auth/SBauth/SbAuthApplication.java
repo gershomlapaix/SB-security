616f5b00a90d9c9006ffae55398fca54e4c827cb
@nsengiyumv33 <nsengiyumv33@gmail.com>
2022-07-09 10:56:19 +0200

    first commit

---
+++ src/main/java/com/auth/SBauth/SbAuthApplication.java
@@ -0,0 +1,13 @@
+package com.auth.SBauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/auth/SBauth/SbAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth.SBauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
