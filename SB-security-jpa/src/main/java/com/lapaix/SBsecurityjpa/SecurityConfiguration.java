025de65b53ff9ecd88125c50f000e8243cdd2f52
@nsengiyumv33 <nsengiyumv33@gmail.com>
2022-07-09 13:39:09 +0200

    my user details and userd details service

--- SB-security-jpa/src/main/java/com/lapaix/SBsecurityjpa/SecurityConfiguration.java
+++ SB-security-jpa/src/main/java/com/lapaix/SBsecurityjpa/SecurityConfiguration.java
@@ -1,13 +1,19 @@
 package com.lapaix.SBsecurityjpa;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Autowired
+    UserDetailsService userDetailsService;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws  Exception{
-        super.configure(auth);
+//        super.configure(auth);
+        auth.userDetailsService(userDetailsService);
     }
 
     @Override
