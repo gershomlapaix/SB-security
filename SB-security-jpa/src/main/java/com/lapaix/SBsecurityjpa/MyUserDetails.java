0c1e8c5ba6a42455e3b2fd4f2d6663abb4e760e0
@nsengiyumv33 <nsengiyumv33@gmail.com>
2022-07-09 15:06:16 +0200

    application properties

--- SB-security-jpa/src/main/java/com/lapaix/SBsecurityjpa/MyUserDetails.java
+++ SB-security-jpa/src/main/java/com/lapaix/SBsecurityjpa/MyUserDetails.java
@@ -1,30 +1,36 @@
 package com.lapaix.SBsecurityjpa;
 
+import com.lapaix.SBsecurityjpa.models.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class MyUserDetails implements UserDetails {
     private String username;
-    public MyUserDetails(String username){
-        this.username = username;
-    }
+    private String password;
+    private boolean active;
+    private List<GrantedAuthority> authorities;
 
-    public MyUserDetails(){
+    public MyUserDetails(User user){
+        this.username = user.getUserName();
+        this.password = user.getPassword();
+        this.active  = user.isActive();
+        this.authorities = Arrays.stream(user.getRoles().split(",")).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
     }
-
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+        return null;
     }
 
     @Override
     public String getPassword() {
-        return "pass";
+        return password;
     }
 
     @Override
--- SB-security-jpa/src/main/java/com/lapaix/SBsecurityjpa/UserRepository.java
+++ SB-security-jpa/src/main/java/com/lapaix/SBsecurityjpa/UserRepository.java
@@ -1,4 +1,10 @@
 package com.lapaix.SBsecurityjpa;
 
-public interface UserRepository {
+import com.lapaix.SBsecurityjpa.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUserName(String userName);
 }
