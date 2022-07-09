fec507d289acb4b8c97cde40204702f6cce6cf30
@nsengiyumv33 <nsengiyumv33@gmail.com>
2022-07-09 14:37:57 +0200

    user model

--- SB-security-jpa/src/main/java/com/lapaix/SBsecurityjpa/MyUserDetails.java
+++ SB-security-jpa/src/main/java/com/lapaix/SBsecurityjpa/MyUserDetails.java
@@ -1,43 +1,54 @@
 package com.lapaix.SBsecurityjpa;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Arrays;
 import java.util.Collection;
 
 public class MyUserDetails implements UserDetails {
+    private String username;
+    public MyUserDetails(String username){
+        this.username = username;
+    }
+
+    public MyUserDetails(){
+    }
+
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return "pass";
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return username;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
---
+++ SB-security-jpa/src/main/java/com/lapaix/SBsecurityjpa/UserRepository.java
@@ -0,0 +1,4 @@
+package com.lapaix.SBsecurityjpa;
+
+public interface UserRepository {
+}
